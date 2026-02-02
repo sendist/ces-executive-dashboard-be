c54e96268863c69a2b793b369b8bf6a2ad423a7d
sendist <sendisetiawan109092@gmail.com>
2026-02-02 10:26:07 +0700

    feat: add helmet for security

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      // Change this: Remove 'unsafe-inline'
+      styleSrc: ["'self'"], 
+      fontSrc: ["'self'"],
+      imgSrc: ["'self'", "data:"],
+      scriptSrc: ["'self'"],
+      frameAncestors: ["'none'"],
+    },
+  },
+  hsts: {
+    maxAge: 31536000, // 1 year
+    includeSubDomains: true,
+    preload: true,
+  },
+}));
 
   app.enableCors({
     origin: process.env.FRONTEND_URL?.split(','),
